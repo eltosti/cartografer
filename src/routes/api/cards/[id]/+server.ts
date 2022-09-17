041096f28d65a98c739dc832b45ad58a1b92d345
agraoltomas <tomasangelotanyo@gmail.com>
2022-09-17 16:57:41 -0300

    agregar GET de carta

--- src/routes/api/cards/[id]/+server.ts
+++ src/routes/api/cards/[id]/+server.ts
@@ -1,9 +1,22 @@
-import type { RequestEvent } from '@sveltejs/kit';
+import { type RequestEvent, json } from "@sveltejs/kit";
+
+import Cards from '$types/cards';
 import type { Card } from "$types/cards";
-import Cards from "$types/cards";
-
-
-export function GET(event: RequestEvent) {
-	console.log(event);
-	let card: Card[] = Cards.filter((card)=> card.id == event.params.id );
+export function GET(event: RequestEvent){
+    let card: Card = Cards.filter((card: Card)=> card.id == event.params.id ).pop();
+    if(card){
+        return json({
+            status: 200, 
+            body: {
+                card
+            }
+        }); 
+    }else{
+        return json({
+            status: 404, 
+            body: {
+                message:"No existe la carta que estas buscando"
+            }
+        })
+    }
 }
