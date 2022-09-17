363c7fe99bbbb3829752d9bbd6778e0cd1e3bc61
agraoltomas <tomasangelotanyo@gmail.com>
2022-09-17 18:42:54 -0300

    cambiar sala de espera a get

--- src/routes/api/rooms/waiting_room/+server.ts
+++ src/routes/api/rooms/waiting_room/+server.ts
@@ -1,11 +1,7 @@
 import type { User } from '$lib/types/user';
 import { store } from '@stores/waiting_room';
 import { error, json, type RequestEvent } from '@sveltejs/kit';
-export async function POST(event: RequestEvent){
-    let { nombre } = await event.request.json();
-    if(!nombre) throw error(400, "Debe indicar un nombre");
-    event.locals.user.nombre = nombre;
-    console.log(event.locals.user);
+export async function GET(event: RequestEvent){
     let localQueue: User[] = [];
     store.subscribe((queue: User[]) => { localQueue = queue });
     store.addUser(event.locals.user);
