aba40f3a3b428d16da1d9ec8a267db0c36bed9ae
agraoltomas <tomasangelotanyo@gmail.com>
2022-09-17 17:49:31 -0300

    limpieza

--- src/routes/api/cards/[id]/+server.ts
+++ src/routes/api/cards/[id]/+server.ts
@@ -1,12 +1,13 @@
 import type { RequestEvent } from '@sveltejs/kit';
-import type { Card } from "src/lib/types/cards";
-import Cards from "src/lib/types/cards";
+import { json } from '@sveltejs/kit';
+import type { Card } from "@types/cards";
+import Cards from "@types/cards";
 import {error} from "@sveltejs/kit";
 
 
 export function GET(event: RequestEvent) {
 	console.log(event);
-	let card: Card = Cards.filter((card: Card)=> card.id == event.params.id ).pop();
-	if (card) return new Response(JSON.stringify(card))
-	throw error("no existe la carta con id "+event.params.id)
+	let card: Card|undefined = Cards.filter((card: Card)=> card.id == event.params.id ).pop();
+	if (card) return json(card);
+	throw error(404, "no existe la carta con id "+event.params.id)
 }
