b2c81f4788c995bcc9911db90aec6c3bfaf558ab
agraoltomas <tomasangelotanyo@gmail.com>
2022-09-18 14:46:32 -0300

    agregar generics

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,13 @@
 import { get_user } from '$lib/types/user';
-import type { RequestEvent, Handle } from '@sveltejs/kit';
-import type { MaybePromise } from '@sveltejs/kit/types/private';
-import type { ResolveOptions } from 'vite';
-export const handle: Handle = async ({ event, resolve }: { 
-	event: RequestEvent; resolve: CallableFunction }) => {
-	let userid = event?.cookies.get('userid');
+import { type RequestEvent, type Handle, redirect } from '@sveltejs/kit';
+export const handle: Handle = async ({ event, resolve }) => {
+	let userid = event.cookies.get('userid');
 	let user   = get_user(userid);
 	if(!userid) {
 		// if this is the first time the user has visited this app,
 		// set a cookie so that we recognise them when they return
 		event.cookies.set("userid", user.userid.toString());
+		throw redirect(301, "");
 	}
 
 	event.locals.user = user;
