c0d872621634dde80e360e0f33b9b0aaeb87e7ed
agraoltomas <tomasangelotanyo@gmail.com>
2022-09-18 20:38:07 -0300

    haciendolo andar

--- src/routes/wait/+page.svelte
+++ src/routes/wait/+page.svelte
@@ -8,14 +8,15 @@
     let queue: WaitingQueue = [];
     onMount(async () => {
         let url = window.location.origin;
-        let r = await fetch(url+"/api/rooms/waiting_room");
+        let r = await fetch("/api/rooms/waiting_room");
         let json = await r.json();
-        console.log(json);
+        queue = json
     });
-    let unsuscribe = store.subscribe((value) => { queue = value.queue });
 
+    
     console.log(queue);
 </script>
+	
 {#each queue as player }
 		<div class="flex">
             {player.nombre}
