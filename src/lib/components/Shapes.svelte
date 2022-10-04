87ef9f93e6b418e960771055c7ee36f1d4f44524
agraoltomas <tomasangelotanyo@gmail.com>
2022-10-04 15:25:42 -0300

    mismo border para todos

--- src/lib/components/Shapes.svelte
+++ src/lib/components/Shapes.svelte
@@ -8,10 +8,10 @@
         [false, false, false, false]
     ];
 
-    const isDiff = (a,x,y) => {
-        if (x < 0 || y<0 || x>3 || y>3) return true
-        return a === shape[x][y]
-    }
+    // const isDiff = (a,x,y) => {
+    //     if (x < 0 || y<0 || x>3 || y>3) return true
+    //     return a === shape[x][y]
+    // }
 
 </script>
 
@@ -19,11 +19,8 @@
     {#each shape as row, x}
         <div class="flex">
             {#each row as col, y}
-                <div class="bg-red-100 p-1 border-black-200
-                        {isDiff(col,x-1,y) ? 'border-l-1':''}
-                        {isDiff(col,x+1,y) ? 'border-r-1':''}
-                        {isDiff(col,x,y-1) ? 'border-b-1':''}
-                        {isDiff(col,x,y+1) ? 'border-t-1':''}
+                <div class="p-1 border-black-700 border
+                        {col ? 'bg-gray-700':'bg-red-100'}
                         "
                 ></div>
             {/each}
