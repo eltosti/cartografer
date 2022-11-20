0f6d6687e9ea7d7ba737d48b06c8ea05415328ce
agraoltomas <tomasangelotanyo@gmail.com>
2022-11-20 17:59:03 -0300

    Generalizando

--- src/stores/board.ts
+++ src/stores/board.ts
@@ -3,15 +3,15 @@ import type { Writable } from 'svelte/store';
 import type {Board, Tile} from '../local_types/store_state';
 import {Terrain} from '../local_types/cards'
 import {emptyTile} from "../local_types/constants";
-
+let BORDER_SIZE = 11;
 
 function createBoard(): { set: (this: void, value: Board) => void; // @ts-ignore
 	subscribe: (this: void, run: Subscriber<Board>, invalidate?: Invalidator<Board>) => Unsubscriber; update: (this: void, updater: Updater<Board>) => void; placeCard: void } {
 
-	let tmp_board = Array(11)
-	for (let i = 0; i < 11; i++) {
+	let tmp_board = Array(BORDER_SIZE)
+	for (let i = 0; i < BORDER_SIZE; i++) {
 		let tmp_row = []
-		for (let j = 0; j < 11; j++) {
+		for (let j = 0; j < BORDER_SIZE; j++) {
 			tmp_row.push(emptyTile)
 		}
 		tmp_board[i]= tmp_row
@@ -19,8 +19,8 @@ function createBoard(): { set: (this: void, value: Board) => void; // @ts-ignore
 
 	const { subscribe, set, update } = writable<Board>({
 		size: {
-			x: 11,
-			y: 11
+			x: BORDER_SIZE,
+			y: BORDER_SIZE
 		},
 		board: tmp_board,
 		mousePosition: {
@@ -49,7 +49,11 @@ function createBoard(): { set: (this: void, value: Board) => void; // @ts-ignore
 			for (let i = 0; i < shape.length; i++) {
 				for (let j = 0; j < shape[i].length; j++) {
 
-					if (shape[i][j] && (typeof(state.board[x+i]) === 'undefined' || typeof(state.board[x+i][y+j]) === 'undefined' || state.board[x+i][y+j].occupied) ){
+					if (shape[i][j] 
+						&& (typeof(state.board[x+i]) === 'undefined' 
+							|| typeof(state.board[x+i][y+j]) === 'undefined' 
+							|| state.board[x+i][y+j].occupied) 
+						){
 						return state
 					}
 				}
